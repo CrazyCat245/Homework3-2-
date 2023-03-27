c3889e8191c4b6a34e753f3c59f3433f85ae5b3c
CrazyCat245 <125860812+CrazyCat245@users.noreply.github.com>
2023-03-27 16:53:41 +0800

    增加選框

--- remake3/MainWindow.xaml.cs
+++ remake3/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace remake3
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
